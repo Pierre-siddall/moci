ffb56f8a22ae29298cbfe5833e48444f0f715425
stephenhaddad <enquiries@metoffice.gov.uk>
2017-03-10 11:42:50 +0000

    MOCI #152: Committed for Harry Shepherd. Merged into /main/trunk: /main/branches/dev/harryshepherd/drivers_0.1_XIOS_NPROC_fix@1659 cf. /main/trunk@1488
    git-svn-id: https://metomi/svn/moci.xm/main/trunk@1771 5d77ff06-061d-4adc-8321-d65833a6d205

--- Coupled_Drivers/xios_driver.py
+++ Coupled_Drivers/xios_driver.py
@@ -24,47 +24,100 @@ import sys
 import error
 import common
 
+def _update_iodef(is_server_mode, is_coupled_mode, iodef_fname='iodef.xml'):
+    '''
+    Update the iodef.xml file for server/attatched mode and couplng mode.
+    is_server_mode and is_coupled_mode are boolean. (true when each option
+    is activated, false otherwise).
+    iodef_fname is a string containing the filename for the iodef file, which
+    defaults to 'iodef.xml'
+    '''
+    # Note we do not use python's xml module for this job, as the comment
+    # line prevalent in the first line of the GO5 iodef.xml files renders
+    # the file invalid as far as the xml module is concerned.
+    swapfile_name = 'swap_iodef'
+    iodef_file = common.open_text_file(iodef_fname, 'r')
+    iodef_swap = common.open_text_file(swapfile_name, 'w')
+    text_bool = ['false', 'true']
+    for line in iodef_file.readlines():
+        # Update the server_mode if the current setting is not what we want
+        if '<!' not in line and 'using_server' in line:
+            line = line.replace(text_bool[not is_server_mode], \
+                                text_bool[is_server_mode])
+        # Update the coupled mode if the current setting is not what we want
+        elif '<!' not in line and 'using_oasis' in line:
+            line = line.replace(text_bool[not is_coupled_mode], \
+                                text_bool[is_coupled_mode])
+
+        iodef_swap.write(line)
+
+    iodef_file.close()
+    iodef_swap.close()
+    os.rename(swapfile_name, iodef_fname)
+
 def _setup_executable(_):
     '''
-    Setup the environment and any files required by the executable
+    Setup the environment and any files required by the executable and/or
+    by the iodef file update procedure.
     '''
     # Load the environment variables required
     xios_envar = common.LoadEnvar()
-    _ = xios_envar.load_envar('XIOS_EXEC', 'unset')
-    _ = xios_envar.load_envar('XIOS_LINK', 'xios.exe')
-    _ = xios_envar.load_envar('XIOS_NPROC', 'unset')
+    if xios_envar.load_envar('XIOS_NPROC') != 0:
+        sys.stderr.write('[FAIL] Environment variable XIOS_NPROC not set '
+                         'Set to 0 to run xios in attatched mode, and to '
+                         'a number greater than 0 for server mode\n')
+        sys.exit(error.MISSING_EVAR_ERROR)
 
-    if xios_envar['XIOS_NPROC'] not in ('0', 'unset'):
+    if xios_envar['XIOS_NPROC'] == '0':
+        # Running in attached mode
+        using_server = False
+    else:
+        # Running in server (detatched) mode
+        # The following environment variables are only relevant for this
+        # mode
+        using_server = True
+        _ = xios_envar.load_envar('XIOS_LINK', 'xios.exe')
         if xios_envar.load_envar('ROSE_LAUNCHER_PREOPTS_XIOS') != 0:
             sys.stderr.write('[FAIL] Environment variable '
                              'ROSE_LAUNCHER_PREOPTS_XIOS not set\n')
             sys.exit(error.MISSING_EVAR_ERROR)
-
-    if xios_envar['XIOS_EXEC'] != 'unset':
+        if xios_envar.load_envar('XIOS_EXEC') != 0:
+            sys.stderr.write('[FAIL] Environment variable XIOS_EXEC '
+                             'not set\n')
+            sys.exit(error.MISSING_EVAR_ERROR)
         if os.path.isfile(xios_envar['XIOS_LINK']):
             os.remove(xios_envar['XIOS_LINK'])
         os.symlink(xios_envar['XIOS_EXEC'],
                    xios_envar['XIOS_LINK'])
 
+    # Check our list of component drivers to see if MCT is active. If it is,
+    # then this is a coupled model. Set the coupler flag accordingly.
+    _ = xios_envar.load_envar('models')
+    using_coupler = 'mct' in xios_envar['models']
+
+    # Update the iodef file
+    _update_iodef(using_server, using_coupler)
+
     return xios_envar
+
 
 def _set_launcher_command(xios_envar):
     '''
     Setup the launcher command for the executable, bearing in mind that XIOS
-    can run attachted. If this is so, this function will return an empty
+    can run attached. If this is so, this function will return an empty
     string
     '''
-    if xios_envar['XIOS_NPROC'] not in ('0', 'unset'):
+
+    if xios_envar['XIOS_NPROC'] != '0':
         launch_cmd = '%s ./%s' % \
             (xios_envar['ROSE_LAUNCHER_PREOPTS_XIOS'], \
                  xios_envar['XIOS_LINK'])
+        # Put in quotes to allow this environment variable to be exported as it
+        # contains (or can contain) spaces
+        xios_envar['ROSE_LAUNCHER_PREOPTS_XIOS'] = "'%s'" % \
+            xios_envar['ROSE_LAUNCHER_PREOPTS_XIOS']
     else:
         launch_cmd = ''
-
-    # Put in quotes to allow this environment variable to be exported as it
-    # contains (or can contain) spaces
-    xios_envar['ROSE_LAUNCHER_PREOPTS_XIOS'] = "'%s'" % \
-        xios_envar['ROSE_LAUNCHER_PREOPTS_XIOS']
 
     return launch_cmd
 
