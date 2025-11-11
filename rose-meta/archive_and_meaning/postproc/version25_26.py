0a1f3c9cd89eae8c37e2d2f76e739d83baf98290
ericaneininger <enquiries@metoffice.gov.uk>
2025-11-11 11:36:43 +0000

    ticket:678 : pierresiddall : archive_integrity app missing metadata
    git-svn-id: https://metomi/svn/moci.xm/main/trunk@5743 5d77ff06-061d-4adc-8321-d65833a6d205

--- rose-meta/archive_and_meaning/postproc/version25_26.py
+++ rose-meta/archive_and_meaning/postproc/version25_26.py
@@ -19,13 +19,25 @@ class UpgradeError(Exception):
       __str__ = __repr__
 
 
-class pp25_tXXX(MacroUpgrade):
+class pp25_t678(MacroUpgrade):
 
-    """Upgrade macro for ticket #XXX by <author>."""
+    """Upgrade macro for ticket #678 by pierresiddall."""
     BEFORE_TAG = "postproc_2.5"
-    AFTER_TAG = "postproc_2.6"
+    AFTER_TAG = "pp25_t678"
 
     def upgrade(self, config, meta_config=None):
         """Upgrade a Postproc app configuration to postproc_2.6."""
+        try:
+            cice_age = self.get_setting_value(config, ["namelist:ciceverify", "cice_age"])
+            self.remove_setting(config, ["namelist:ciceverify", "cice_age"])
+        except AttributeError:
+            cice_age = "false"
+        
+        self.add_setting(config,["namelist:ciceverify","cice_age_rst"],cice_age)
+        base = self.get_setting_value(config, ["namelist:nemo_processing", "base_component"])
+        self.add_setting(config,["namelist:nemoverify","base_mean"],base)
+        self.add_setting(config,["namelist:nemoverify","nemo_version"],"4.2+")
+        self.add_setting(config,["namelist:nemoverify","nemo_ice_rst"],"false")
+        self.add_setting(config,["namelist:nemoverify","nemo_icb_rst"],"false")
         
         return config, self.reports
