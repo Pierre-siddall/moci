f8b0a3a62515fae26fadf639ce19455810307fd9
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-20 11:38:14 +0000

    Implement basic logging

--- Utilities/lib/shellout.py
+++ Utilities/lib/shellout.py
@@ -2,6 +2,7 @@ import timer
 import subprocess
 import shlex
 import os
+import sys
 
 
 def _exec_subprocess(cmd, verbose=True,current_working_directory=os.getcwd()):
@@ -9,14 +10,22 @@ def _exec_subprocess(cmd, verbose=True,current_working_directory=os.getcwd()):
     Execute a given shell command
 
     :param cmd: The command to be executed given as a string
-    :param verbose: A boolean value to determine if the stout
+    :param verbose: A boolean value to determine if the stdout
     stream is displayed during the runtime.
     :param current_working_directory: The directory in which the
     command should be executed.
     """
-    process = subprocess.run(cmd,
+    output = subprocess.run(cmd,
                              stdin= subprocess.PIPE,
                              capture_output=True,
                              cwd=current_working_directory,
                              timeout=10
                              )
+    if verbose and output:
+        sys.stdout.write(f"[DEBUG]{output.stdout}\n")
+    if output.stderr and output.returncode!= 0:
+        sys.stderr.write(f"[ERROR] {output.stderr}\n")
+    if sys.version_info[0] >= 3:
+        output.stdout = output.stdout.decode()
+
+    return output
