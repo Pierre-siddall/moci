d3aafae76a3c3aebd833490fd539207b7b32bb5f
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-20 15:16:18 +0000

    Update copyright statements

--- Coupled_Drivers/si3_controller.py
+++ Coupled_Drivers/si3_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 *****************************COPYRIGHT******************************
- (C) Crown copyright 2021-2025 Met Office. All rights reserved.
+ (C) Crown copyright 2021-2026 Met Office. All rights reserved.
  Use, duplication or disclosure of this code is subject to the restrictions
  as set forth in the licence. If no licence has been raised with this copy
  of the code, the use, duplication or disclosure of it is strictly
